2665d18b28a3e429673f9708c9a8607578c2d5c5
Stepan Suvorov <stevermeister@gmail.com>
2020-04-07 14:50:56 +0200

    provideIn:platform example

---
+++ src/app/first/first.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { FirstComponent } from './first.component';
+import { BrowserModule } from '@angular/platform-browser';
+
+
+
+@NgModule({
+  declarations: [
+    FirstComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  bootstrap: [FirstComponent]
+})
+export class FirstModule { }
